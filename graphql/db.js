5d918cc6910e66cd210ea4515b6dfb98deb6dc25
taerikor <akdud153@gmail.com>
2021-04-27 03:08:25 +0900

    creating mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,49 @@
-export const people = [
+let movies = [
     {
     id:0,
-    name: "Taeri",
-    age: 25,
-    gender: 'male'
+    name: "The Shawshank Redemption",
+    score: 10,
     },
     {
     id:1,
-    name: "Sejin",
-    age: 28,
-    gender: 'female'
+    name: "Forrest Gump",
+    score: 30,
     },
     {
     id:2,
-    name: "Homin",
-    age: 26,
-    gender: 'male'
+    name: "Schindler's List",
+    score: 40,
+    },
+    {
+    id:3,
+    name: "The Godfather",
+    score: 20,
     }
 ]
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => person.id === id)
-    return filteredPeople[0]
+    const filteredMovie = movies.filter(movie => movie.id === id)
+    return filteredMovie[0]
 } 
+
+export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id)
+    if( movies.length > cleanedMovies.length){
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false
+    }
+}
+
+export const addMovie = (name,score) => {
+    const newMovie = {
+        id: `${movies.length + 1}`,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+}
